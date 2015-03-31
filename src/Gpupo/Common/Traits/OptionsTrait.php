5acbaf80862ef0162f81a0654d8e6fea2e291963
Gilmar Pupo <g@g1mr.com>
2015-03-31 09:27:00 -0300

    PHP CS fix

--- src/Gpupo/Common/Traits/OptionsTrait.php
+++ src/Gpupo/Common/Traits/OptionsTrait.php
@@ -1,4 +1,14 @@
 <?php
+
+/*
+ * This file is part of common
+ *
+ * (c) Gilmar Pupo <g@g1mr.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Gpupo\Common\Traits;
 
 use Gpupo\Common\Entity\Collection;
@@ -12,15 +22,15 @@ trait OptionsTrait
         if (!$this->options instanceof Collection) {
             $this->setOptions($this->options);
         }
-        
+
         return $this->options;
     }
-    
+
     public function getDefaultOptions()
     {
         return [];
     }
-    
+
     public function setOptions(Array $options = [])
     {
         $list = array_merge($this->getDefaultOptions(), $options);
--- tests/Gpupo/Tests/Common/Entity/CollectionTest.php
+++ tests/Gpupo/Tests/Common/Entity/CollectionTest.php
@@ -1,18 +1,27 @@
 <?php
 
+/*
+ * This file is part of common
+ *
+ * (c) Gilmar Pupo <g@g1mr.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Gpupo\Tests\Common\Entity;
 
-use Gpupo\Tests\TestCaseAbstract;
 use Gpupo\Common\Entity\Collection;
+use Gpupo\Tests\TestCaseAbstract;
 
-class CommonTest extends TestCaseAbstract
+class CollectionTest extends TestCaseAbstract
 {
     /**
      * @dataProvider dataProviderInformacao
      */
     public function testPossuiEstruturaDeInformacao($value)
     {
-        $collection = new Collection;
+        $collection = new Collection();
         $collection->set('foo', $value);
         $this->assertEquals($value, $collection->get('foo'));
     }
