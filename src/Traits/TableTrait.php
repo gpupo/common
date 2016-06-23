b0d9125d3db1751a3bcfa8044aff7a6f803b6c71
Gilmar Pupo <g@g1mr.com>
2016-06-23 16:27:43 -0300

    Table

---
+++ src/Traits/TableTrait.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * This file is part of gpupo/common
+ *
+ * (c) Gilmar Pupo <g@g1mr.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * For more information, see
+ * <http://www.g1mr.com/common/>.
+ */
+namespace Gpupo\Common\Traits;
+
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Output\OutputInterface;
+
+trait TableTrait
+{
+    public function displayTableResults(OutputInterface $output, $object, array $keysOnly = [], $maxWidth = 35)
+    {
+        $table = new Table($output);
+        $table->setStyle('borderless');
+        $list = $object->toArray();
+
+        foreach ($list as $item) {
+            foreach ($item as $key => $value) {
+                if (!empty($keysOnly) && !in_array($key, $keysOnly, true)) {
+                    unset($item[$key]);
+
+                    continue;
+                }
+
+                if (is_array($value)) {
+                    $value = json_encode($value);
+                }
+                if (is_float($value)) {
+                    $value = number_format($value, 3);
+                }
+                $value = str_replace(
+                    ['["', '"]', '{"', '"', '}'],
+                    [],
+                    $value
+                );
+
+                if (empty($value)) {
+                    $value = '~';
+                }
+
+                $value = substr($value, 0, $maxWidth);
+
+                $item[$key] = $value;
+            }
+
+            if (!isset($headers)) {
+                $headers = array_keys($item);
+                $table->setHeaders($headers);
+            }
+
+            $table->addRow($item);
+        }
+
+        return $table->render($output);
+    }
+}
