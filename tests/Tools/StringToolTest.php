351f28d3c918f85a743141a0b294b7a37e1b9b5c
Gilmar Pupo <g@g1mr.com>
2016-06-27 15:12:39 -0300

    doc files

--- tests/Tools/StringToolTest.php
+++ tests/Tools/StringToolTest.php
@@ -2,15 +2,12 @@
 
 /*
  * This file is part of gpupo/common
- *
- * (c) Gilmar Pupo <g@g1mr.com>
- *
+ * Created by Gilmar Pupo <g@g1mr.com>
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
- *
- * For more information, see
- * <http://www.g1mr.com/common/>.
+ * For more information, see <http://www.g1mr.com/>.
  */
+
 namespace Gpupo\Tests\Common\Tools;
 
 use Gpupo\Common\Tools\StringTool;
