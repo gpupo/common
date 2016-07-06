1b94f6936a63469cadc365f99723d28ca41a6926
Gilmar Pupo <g@g1mr.com>
2016-07-06 14:28:10 -0300

    tests

---
+++ tests/Entity/ArrayCollectionTest.php
@@ -0,0 +1,250 @@
+<?php
+
+/*
+ * This file is part of gpupo/common
+ * Created by Gilmar Pupo <g@g1mr.com>
+ * For the information of copyright and license you should read the file
+ * LICENSE which is distributed with this source code.
+ * Para a informação dos direitos autorais e de licença você deve ler o arquivo
+ * LICENSE que é distribuído com este código-fonte.
+ * Para obtener la información de los derechos de autor y la licencia debe leer
+ * el archivo LICENSE que se distribuye con el código fuente.
+ * For more information, see <http://www.g1mr.com/>.
+ */
+
+namespace Gpupo\Tests\Common\Entity;
+
+use Gpupo\Common\Entity\ArrayCollection;
+use Gpupo\Tests\Common\TestCaseAbstract;
+
+/**
+ * Minimal Based version of the test of Doctrine\Common\Collections\ArrayCollection
+ * For more information, see <http://www.doctrine-project.org>.
+ */
+class ArrayCollectionTest extends TestCaseAbstract
+{
+    /**
+     * @dataProvider provideDifferentElements
+     */
+    public function testToArray($elements)
+    {
+        $collection = new ArrayCollection($elements);
+
+        $this->assertSame($elements, $collection->toArray());
+    }
+
+    /**
+     * @dataProvider provideDifferentElements
+     */
+    public function testFirst($elements)
+    {
+        $collection = new ArrayCollection($elements);
+        $this->assertSame(reset($elements), $collection->first());
+    }
+
+    /**
+     * @dataProvider provideDifferentElements
+     */
+    public function testLast($elements)
+    {
+        $collection = new ArrayCollection($elements);
+        $this->assertSame(end($elements), $collection->last());
+    }
+
+    /**
+     * @dataProvider provideDifferentElements
+     */
+    public function testKey($elements)
+    {
+        $collection = new ArrayCollection($elements);
+
+        $this->assertSame(key($elements), $collection->key());
+
+        next($elements);
+        $collection->next();
+
+        $this->assertSame(key($elements), $collection->key());
+    }
+
+    /**
+     * @dataProvider provideDifferentElements
+     */
+    public function testNext($elements)
+    {
+        $collection = new ArrayCollection($elements);
+
+        while (true) {
+            $collectionNext = $collection->next();
+            $arrayNext = next($elements);
+
+            if (!$collectionNext || !$arrayNext) {
+                break;
+            }
+
+            $this->assertSame($arrayNext, $collectionNext, 'Returned value of ArrayCollection::next() and next() not match');
+            $this->assertSame(key($elements), $collection->key(), 'Keys not match');
+            $this->assertSame(current($elements), $collection->current(), 'Current values not match');
+        }
+    }
+
+    /**
+     * @dataProvider provideDifferentElements
+     */
+    public function testCurrent($elements)
+    {
+        $collection = new ArrayCollection($elements);
+
+        $this->assertSame(current($elements), $collection->current());
+
+        next($elements);
+        $collection->next();
+
+        $this->assertSame(current($elements), $collection->current());
+    }
+
+    /**
+     * @dataProvider provideDifferentElements
+     */
+    public function testGetKeys($elements)
+    {
+        $collection = new ArrayCollection($elements);
+
+        $this->assertSame(array_keys($elements), $collection->getKeys());
+    }
+
+    /**
+     * @dataProvider provideDifferentElements
+     */
+    public function testGetValues($elements)
+    {
+        $collection = new ArrayCollection($elements);
+
+        $this->assertSame(array_values($elements), $collection->getValues());
+    }
+
+    /**
+     * @dataProvider provideDifferentElements
+     */
+    public function testCount($elements)
+    {
+        $collection = new ArrayCollection($elements);
+
+        $this->assertSame(count($elements), $collection->count());
+    }
+
+    /**
+     * @return array
+     */
+    public function provideDifferentElements()
+    {
+        return [
+            'indexed'     => [[1, 2, 3, 4, 5]],
+            'associative' => [['A' => 'a', 'B' => 'b', 'C' => 'c']],
+            'mixed'       => [['A' => 'a', 1, 'B' => 'b', 2, 3]],
+        ];
+    }
+
+    public function testRemove()
+    {
+        $elements = [1, 'A' => 'a', 2, 'B' => 'b', 3];
+        $collection = new ArrayCollection($elements);
+
+        $this->assertSame(1, $collection->remove(0));
+        unset($elements[0]);
+
+        $this->assertSame(null, $collection->remove('non-existent'));
+        unset($elements['non-existent']);
+
+        $this->assertSame(2, $collection->remove(1));
+        unset($elements[1]);
+
+        $this->assertSame('a', $collection->remove('A'));
+        unset($elements['A']);
+
+        $this->assertSame($elements, $collection->toArray());
+    }
+
+    public function testRemoveElement()
+    {
+        $elements = [1, 'A' => 'a', 2, 'B' => 'b', 3, 'A2' => 'a', 'B2' => 'b'];
+        $collection = new ArrayCollection($elements);
+
+        $this->assertTrue($collection->removeElement(1));
+        unset($elements[0]);
+
+        $this->assertFalse($collection->removeElement('non-existent'));
+
+        $this->assertTrue($collection->removeElement('a'));
+        unset($elements['A']);
+
+        $this->assertTrue($collection->removeElement('a'));
+        unset($elements['A2']);
+
+        $this->assertSame($elements, $collection->toArray());
+    }
+
+    public function testContainsKey()
+    {
+        $elements = [1, 'A' => 'a', 2, 'null' => null, 3, 'A2' => 'a', 'B2' => 'b'];
+        $collection = new ArrayCollection($elements);
+
+        $this->assertTrue($collection->containsKey(0), 'Contains index 0');
+        $this->assertTrue($collection->containsKey('A'), 'Contains key "A"');
+        $this->assertTrue($collection->containsKey('null'), 'Contains key "null", with value null');
+        $this->assertFalse($collection->containsKey('non-existent'), "Doesn't contain key");
+    }
+
+    public function testEmpty()
+    {
+        $collection = new ArrayCollection();
+        $this->assertTrue($collection->isEmpty(), 'Empty collection');
+
+        $collection->add(1);
+        $this->assertFalse($collection->isEmpty(), 'Not empty collection');
+    }
+
+    public function testContains()
+    {
+        $elements = [1, 'A' => 'a', 2, 'null' => null, 3, 'A2' => 'a', 'zero' => 0];
+        $collection = new ArrayCollection($elements);
+
+        $this->assertTrue($collection->contains(0), 'Contains Zero');
+        $this->assertTrue($collection->contains('a'), 'Contains "a"');
+        $this->assertTrue($collection->contains(null), 'Contains Null');
+        $this->assertFalse($collection->contains('non-existent'), "Doesn't contain an element");
+    }
+
+    public function testExists()
+    {
+        $elements = [1, 'A' => 'a', 2, 'null' => null, 3, 'A2' => 'a', 'zero' => 0];
+        $collection = new ArrayCollection($elements);
+
+        $this->assertTrue($collection->exists(function ($key, $element) {
+            return $key === 'A' && $element === 'a';
+        }), 'Element exists');
+
+        $this->assertFalse($collection->exists(function ($key, $element) {
+            return $key === 'non-existent' && $element === 'non-existent';
+        }), 'Element not exists');
+    }
+
+    public function testIndexOf()
+    {
+        $elements = [1, 'A' => 'a', 2, 'null' => null, 3, 'A2' => 'a', 'zero' => 0];
+        $collection = new ArrayCollection($elements);
+
+        $this->assertSame(array_search(2, $elements, true), $collection->indexOf(2), 'Index of 2');
+        $this->assertSame(array_search(null, $elements, true), $collection->indexOf(null), 'Index of null');
+        $this->assertSame(array_search('non-existent', $elements, true), $collection->indexOf('non-existent'), 'Index of non existent');
+    }
+
+    public function testGet()
+    {
+        $elements = [1, 'A' => 'a', 2, 'null' => null, 3, 'A2' => 'a', 'zero' => 0];
+        $collection = new ArrayCollection($elements);
+
+        $this->assertSame(2, $collection->get(1), 'Get element by index');
+        $this->assertSame('a', $collection->get('A'), 'Get element by name');
+        $this->assertSame(null, $collection->get('non-existent'), 'Get non existent element');
+    }
+}
