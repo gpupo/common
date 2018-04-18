2f61bec2ae8792cac23b154a7591452c404f6247
Gilmar Pupo <contact@gpupo.com>
2018-04-18 08:35:08 -0300

    Logger channel switch

--- src/Traits/LoggerAwareTrait.php
+++ src/Traits/LoggerAwareTrait.php
@@ -17,20 +17,29 @@ declare(strict_types=1);
 
 namespace Gpupo\Common\Traits;
 
+use Psr\Log\LoggerInterface;
+use Psr\Log\LoggerAwareTrait as CoreLoggerAwareTrait;
+
 trait LoggerAwareTrait
 {
-    use \Psr\Log\LoggerAwareTrait;
-
+    use CoreLoggerAwareTrait;
+    
     public function getLogger()
     {
         return $this->logger;
     }
 
-    public function initLogger($logger)
+    public function initLogger($logger, $name = null)
     {
-        if (!empty($logger)) {
-            return $this->setLogger($logger);
+        if (empty($logger)) {
+            return;
         }
+
+        if (!empty($name)) {
+            $logger = $logger->withName($name);
+        }
+
+        return $this->setLogger($logger);
     }
 
     public function log($level, $message, array $context = [])
