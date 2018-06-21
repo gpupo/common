e4bee4fc1288115196dc3a0a7e99c2452e93bb77
Gilmar Pupo <contact@gpupo.com>
2018-06-21 09:06:08 -0300

    fix alias

--- bin/alias.sh
+++ bin/alias.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 alias phpunit='docker-compose run php-dev vendor/bin/phpunit'
-alias php-cs-fixer='docker-compose run php-dev /root/.composer/vendor/bin/php-cs-fixer fix'
+alias php-cs-fixer='docker-compose run php-dev /root/.composer/vendor/bin/php-cs-fixer'
