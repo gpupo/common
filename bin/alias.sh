27e0f33b1136eb4a0d6e38188f0361000113e7b7
Gilmar Pupo <contact@gpupo.com>
2018-07-13 09:58:16 -0300

    Update alias

--- bin/alias.sh
+++ bin/alias.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-alias phpunit='docker-compose run php-dev vendor/bin/phpunit'
+alias php-dev='docker-compose run php-dev bash'
+alias php-dev-phpunit='docker-compose run php-dev vendor/bin/phpunit'
 alias php-cs-fixer='docker-compose run php-dev /root/.composer/vendor/bin/php-cs-fixer'
+alias phpcbf='docker-compose run php-dev /root/.composer/vendor/bin/phpcbf'
+alias phpcs='docker-compose run php-dev /root/.composer/vendor/bin/phpcs'
