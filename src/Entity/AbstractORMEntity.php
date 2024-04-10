ccc13897a1a87dedbff49dee2df526a5c09c4a08
Bruno Correa <bruno.almeida@nvpc.io>
2024-04-10 14:15:45 -0300

    Upgrade de pacotes para Symfony 6 e ajustes para compatibilidade com PHP 8.3

--- src/Entity/AbstractORMEntity.php
+++ src/Entity/AbstractORMEntity.php
@@ -13,23 +13,24 @@ namespace Gpupo\Common\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Gpupo\Common\Traits\PropertyAccessorsTrait;
 
-/**
- * @ORM\MappedSuperclass
- */
+#[ORM\MappedSuperclass]
 abstract class AbstractORMEntity implements CollectionInterface
 {
     use PropertyAccessorsTrait;
 
+    #[\Override]
     public function toJson(string $route = null, int $options = 0, int $depth = 512): string
     {
         return '';
     }
 
+    #[\Override]
     public function toLog(): array
     {
         return [];
     }
 
+    #[\Override]
     public function toArray(): array
     {
         return [];
